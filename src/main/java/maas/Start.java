dfb7a6d22ced646e7a93e4dcc8a08d4a98a7a30d
Angela Enriquez <anenriqu@outlook.com>
2018-10-26 10:01:40 +0200

    Initializing a list of buyers and sellers agents

--- src/main/java/maas/Start.java
+++ src/main/java/maas/Start.java
@@ -3,11 +3,32 @@ package maas;
 import java.util.List;
 import java.util.Vector;
 import maas.tutorials.BookBuyerAgent;
+import maas.tutorials.BookSellerAgent;
 
 public class Start {
     public static void main(String[] args) {
+        int nBookBuyers = 20;
+        int nBookSellers = 3;
+
     	List<String> agents = new Vector<>();
-    	agents.add("tester:maas.tutorials.BookBuyerAgent");
+
+        for (int i=0; i< nBookBuyers; i++){
+            StringBuilder sb = new StringBuilder();
+            sb.append("Buyer_");
+            sb.append(Integer.toString(i));
+            sb.append(":maas.tutorials.BookBuyerAgent");
+            agents.add(sb.toString());
+        }
+
+        for (int i=0; i< nBookSellers; i++){
+            StringBuilder sb = new StringBuilder();
+            sb.append("Seller_");
+            sb.append(Integer.toString(i));
+            sb.append(":maas.tutorials.BookSellerAgent");
+            agents.add(sb.toString());
+        }
+
+    	//agents.add("tester:maas.tutorials.BookBuyerAgent");
 
     	List<String> cmd = new Vector<>();
     	cmd.add("-agents");
